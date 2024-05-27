490d0bbb5edc32c56a44d6952e7f59e8d9b6094e
ryywh45 <ryywh45@gmail.com>
2024-05-27 23:45:46 +0800

    complete first version

--- main/uart/uartHandler.py
+++ main/uart/uartHandler.py
@@ -1,27 +1,95 @@
 import asyncio
-import websockets
-import serial
+import websockets.server as websockets
+from serial import Serial, SerialException
 import json
 
-async def websocket_server(websocket, path):
+#=================================================================#
+
+wsConnections: dict[str, websockets.WebSocketServerProtocol] = {}
+
+async def wsHandler(websocket: websockets.WebSocketServerProtocol):
     """deal with incoming messages from webSocket"""
     try:
         async for data in websocket:
-            print(f"From webSocket <- {data}")
-            await serial_write(json.loads(data))
-    except websockets.exceptions.ConnectionClosedError as e:
-        print(f"WebSocket connection closed: {e}")
+            try: 
+                service, service_data = data.split(' : ')
+            except Exception: 
+                print('Invalid data from webSocket')
+                continue
+            print(f"From {service} <- {data}")
+            if service_data == 'Hello':
+                wsConnections[service] = websocket
+                continue
+
+            if service == '[ test.py ]':
+                result = await serial_write(json.loads(service_data))
+                if result is True:
+                    print(f'To {service} -> ok')
+                    await websocket.send('ok')
+                else:
+                    print(f'To {service} -> error')
+                    await websocket.send('error')
+
+            elif service == '[ test2.py ]':
+                result = await serial_write(json.loads(service_data))
+                if result is True:
+                    print(f'To {service} -> ok')
+                    await websocket.send('ok')
+                else:
+                    print(f'To {service} -> error')
+                    await websocket.send('error')
+
+            elif service == '[ picam_record.py ]':
+                pass
+
     except Exception as e:
-        print(f"Error in websocket_server: {e}")
+        print(f"Error in [ wsHandler() ] : {e}")
+
+    finally:
+        if service in wsConnections:
+            del wsConnections[service]
+
+async def wsSend(service, data):
+    """send data to webSocket"""
+    try:
+        print(f"To {service} -> {data}")
+        await wsConnections[service].send(data)
+
+    except KeyError:
+        print(f"Error in [ wsSend() ] : {service} not connected")
+    
+    except Exception as e:
+        print(f"Error in [ wsSend() ] : {e}")
+
+#=================================================================#
 
 async def serial_reader():
     """deal with incoming messages from serial"""
+    await asyncio.sleep(3)
+    loop = asyncio.get_event_loop()
+    print("Serial reader start")
+
     try:
         while True:
             data = await loop.run_in_executor(None, ser.readline)
+            if list(data) == []: continue
+
             print(f"From serial <- {data}={list(data)}")
+            if data[1] == 2:                                          # code 2 = control picam
+                if data[2] == 1:
+                    await wsSend('[ picam_record.py ]', 'go')               # start recording
+                elif data[2] == 2:
+                    await wsSend('[ picam_record.py ]', 'stop')             # stop recording
+                else:
+                    pass
+            else:
+                pass
+
     except Exception as e:
-        print(f"Error in serial_reader: {e}")
+        print(f"Error in [ serial_reader() ] : {e}")
+
+    finally:
+        ser.close()
 
 async def serial_write(data):
     """process data and send it to serial"""
@@ -31,28 +99,30 @@ async def serial_write(data):
         msg.append(checksum // 256)
         msg.append(checksum % 256)
         msg.append(ord('\n'))
-        print(f"To serial -> {msg}={[chr(i) for i in msg]}")
         ser.write(bytes(msg))
+        print(f"To serial -> {msg}={[chr(i) for i in msg]}")
+
     except Exception as e:
-        print(f"Error in serial_write: {e}")
+        print(f"Error in [ serial_write() ] : {e}")
+        return False
+
+    return True
+
+#=================================================================#
 
 async def main():
-    # websocket_server_task = websockets.serve(websocket_server, "localhost", 8000)
-    # await websocket_server_task
     print("WebSocket server start")
+    async with websockets.serve(wsHandler, "localhost", 8000):
+        await serial_reader()
 
-    await asyncio.gather(
-        websockets.serve(websocket_server, "localhost", 8000),
-        serial_reader()
-    )
 
 if __name__ == "__main__":
     try:
-        ser = serial.Serial('/dev/serial0', 115200)
-    except serial.SerialException as err:
+        ser = Serial('/dev/serial0', 115200, timeout=1)
+        ser.flush()
+
+    except SerialException as err:
         print(f"Failed to open serial port: {err}")
         exit(1)
 
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(main())
-    loop.run_forever()
+    asyncio.run(main())
