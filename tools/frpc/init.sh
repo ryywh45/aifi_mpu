14a59127d73ee68abcee5f47d8554e84b8f938c0
ryywh45 <ryywh45@gmail.com>
2024-07-12 22:35:07 +0800

    fix file path issue

--- tools/frpc/init.sh
+++ tools/frpc/init.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+USER_HOME=$(eval echo ~$USER)
+sed -i "s|\$HOME|$USER_HOME|g" "frp-client.service"
+sed -i "s|\$HOME|$USER_HOME|g" "run.sh"
 systemctl stop frp-client.service
 cp frp-client.service /etc/systemd/system/frp-client.service
 systemctl start frp-client.service
