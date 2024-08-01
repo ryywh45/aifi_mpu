313e875b1e02b6da5df82e924aa1154ab92c4231
ryywh45 <ryywh45@gmail.com>
2024-08-01 11:53:03 +0800

    update get method

--- tools/fish-eyes/cmd/get.go
+++ tools/fish-eyes/cmd/get.go
@@ -5,7 +5,12 @@ import (
 	"fish-eyes/internal/fissh"
 )
 
-var filename string
+var filenameOption  fissh.Option = fissh.Option{Name: fissh.OptionFilename}
+var dateOption      fissh.Option = fissh.Option{Name: fissh.OptionDate}
+var latestVidOption fissh.Option = fissh.Option{Name: fissh.OptionLatestVid}
+var latestPicOption fissh.Option = fissh.Option{Name: fissh.OptionLatestPic}
+const defaultDate string = "20240101"
+
 var dstPath string
 
 var getCmd = &cobra.Command{
@@ -14,7 +19,16 @@ var getCmd = &cobra.Command{
 	Long: ``,
 	Run: func(cmd *cobra.Command, args []string) {
 		fissh.ChangeUsername(username)
-		fissh.GetFishMedia(fishID, filename, dstPath)
+		switch {
+		case filenameOption.Value != "":
+			fissh.GetFishMedia(fishID, filenameOption, dstPath)
+		case dateOption.Value != "":
+			fissh.GetFishMedia(fishID, dateOption, dstPath)
+		case latestVidOption.Value != "":
+			fissh.GetFishMedia(fishID, latestVidOption, dstPath)
+		case latestPicOption.Value != "":
+			fissh.GetFishMedia(fishID, latestPicOption, dstPath)
+		}
 	},
 }
 
@@ -22,7 +36,13 @@ func init(){
 	rootCmd.AddCommand(getCmd)
 	getCmd.Flags().StringVar(&fishID, "id", "", "specify fish id, this is required")
 	getCmd.MarkFlagRequired("id")
-	getCmd.Flags().StringVarP(&filename, "name", "n", "", "specify the name of file")
+
+	getCmd.Flags().StringVarP(&filenameOption.Value, "name", "n", "", "specify the name of file")
+	getCmd.Flags().StringVarP(&dateOption.Value, "date", "d", "", "specify file date, ex:" + defaultDate)
+	getCmd.Flags().StringVar(&latestVidOption.Value, "latestVid", "", "specify the number of latest vids to get")
+	getCmd.Flags().StringVar(&latestPicOption.Value, "latestPic", "", "specify the number of latest pics to get")
+	getCmd.MarkFlagsMutuallyExclusive("name", "date", "latestVid", "latestPic")
+
 	getCmd.Flags().StringVar(&dstPath, "path", ".", "specify destination path")
 	getCmd.Flags().StringVar(&username, "username", "aifi-fish", "change username")
 }
--- tools/fish-eyes/internal/fissh/fissh.go
+++ tools/fish-eyes/internal/fissh/fissh.go
@@ -15,6 +15,20 @@ import (
 var vidPath string = "/home/" + username + "/aifi_mpu/main/service/picam_record/videos"
 var picPath string = "/home/" + username + "/aifi_mpu/main/service/picam_record/pictures"
 
+type optionName int
+
+const (
+	OptionFilename optionName = iota
+	OptionDate
+	OptionLatestVid
+	OptionLatestPic
+)
+
+type Option struct {
+	Name optionName
+	Value string
+}
+
 func ListFishMedia(id string, vid bool, pic bool) string {
 	client := connectById(id)
 	defer client.Close()
@@ -42,15 +56,36 @@ func ListFishMedia(id string, vid bool, pic bool) string {
 	return b.String()
 }
 
-func GetFishMedia(id string, filename string, dstPath string) {
+func ListFishMediaSlice(id string, vid, pic bool) []string {
+	rawStr := ListFishMedia(id, vid, pic)
+	slice := []string{}
+	for _, str := range strings.Split(rawStr, "\n") {
+		if strings.HasPrefix(str, "202") {
+			slice = append(slice, str)
+		}
+	}
+	return slice
+}
+
+func GetFishMedia(id string, option Option, dstPath string) {
 	var srcPath string
-	switch {
-	case strings.Contains(filename, ".mp4"):
-		srcPath = vidPath
-	case strings.Contains(filename, ".png"):
-		srcPath = picPath
+	var filenames []string
+
+	switch option.Name {
+	case OptionFilename:
+		filenames = []string{option.Value}
+	case OptionDate:
+		filenames = getByDate(id, option.Value)
+	case OptionLatestVid:
+		filenames = getByLatestVid(id, option.Value)
+	case OptionLatestPic:
+		filenames = getByLatestPic(id, option.Value)
 	default:
-		log.Fatal("Invaild filename: ", filename)
+		log.Fatal("Invaild option")
+	}
+
+	if len(filenames) == 0 {
+		log.Fatal("No maching files found.")
 	}
 
 	client := connectById(id)
@@ -61,8 +96,46 @@ func GetFishMedia(id string, filename string, dstPath string) {
 		log.Fatal(err)
 	}
 	defer sftpClient.Close()
-	
-	copySingleFile(sftpClient, filename, srcPath, dstPath)
+
+	for _, filename := range filenames {
+		switch {
+		case strings.Contains(filename, ".mp4"):
+			srcPath = vidPath
+		case strings.Contains(filename, ".png"):
+			srcPath = picPath
+		default:
+			continue
+		}
+		copySingleFile(sftpClient, filename, srcPath, dstPath)
+	}
+}
+
+func getByDate(id, date string) []string {
+	var filenames []string
+	for _, filename := range ListFishMediaSlice(id, true, true) {
+		if strings.HasPrefix(filename, date) {
+			filenames = append(filenames, filename)
+		}
+	}
+	return filenames
+}
+
+func getByLatestVid(id, count string) []string {
+	var filenames []string = ListFishMediaSlice(id, true, false)
+	c, err := strconv.Atoi(count)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return filenames[len(filenames) - c:] 
+}
+
+func getByLatestPic(id, count string) []string {
+	var filenames []string = ListFishMediaSlice(id, false, true)
+	c, err := strconv.Atoi(count)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return filenames[len(filenames) - c:] 
 }
 
 func getFishPort(id string) (string, error) {
