c1b204c53afb38337f0f81529fc9f7f6ce338f07
Your Name <you@example.com>
2024-10-23 15:56:35 +0800

    new

--- main/service/picam_recog/recog_video.py
+++ main/service/picam_recog/recog_video.py
@@ -152,7 +152,8 @@ async def InferenceTensorFlow(ws, result, image, model, output, label=None):
     if Nothingnum >= 29:
         print("Nothing R2")
         current_time = datetime.now().strftime('%H:%M:%S')
-        command_history.append(("NothingR2", current_time, ""))
+        coordinates_message = "X:0 Y:0"
+        command_history.append(("NoR2", current_time, coordinates_message))
         await ws.send(WebsocketMsg(NAME, {"toSerial":
             [ord("R"), ord("2"), 0, 0]}).to_json())
         await asyncio.sleep(0.1)
