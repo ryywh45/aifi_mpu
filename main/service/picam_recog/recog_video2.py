703007cff80d213d6c036c33f2a331ec92c31911
Your Name <you@example.com>
2024-11-13 17:56:32 +0800

    new

--- main/service/picam_recog/recog_video2.py
+++ main/service/picam_recog/recog_video2.py
@@ -23,6 +23,8 @@ Detectnum = 0
 IsSteady = False
 already_up = False
 already_down = False
+Steadyflag = False
+last_Xmin, last_Ymin, last_Xmax, last_Ymax = 0, 0, 0, 0
 Nothingnum = 0
 NAME = 'picam_recog.py'
 modelPath = "./model/model2.tflite"
@@ -33,8 +35,18 @@ should_stop = True
 fourcc = cv2.VideoWriter_fourcc(*'mp4v')
 out = None  
 command_history = []
-def save_command_history_to_csv(filename='command_history.csv'):
 
+interpreter = None
+input_details, output_details = None, None
+
+async def InitializeModel(model_path):
+    global interpreter, input_details, output_details
+    interpreter = tflite.Interpreter(model_path=model_path, num_threads=4)
+    interpreter.allocate_tensors()
+    input_details = interpreter.get_input_details()
+    output_details = interpreter.get_output_details()
+
+def save_command_history_to_csv(filename=f"{datetime.now().strftime('%Y%m%d_%H%M%S')}_command.csv"):
     filename = os.path.expanduser(filename)
 
     with open(filename, mode='w', newline='') as file:
@@ -43,20 +55,17 @@ def save_command_history_to_csv(filename='command_history.csv'):
         writer.writerow(["Command", "Timestamp", "Coordinates"])
 
         for command, timestamp, coordinates in command_history:
-
             if isinstance(timestamp, str):
                 try:
-                    timestamp = datetime.strptime(timestamp, '%Y-%m-%d %H:%M:%S')
+                    timestamp = datetime.strptime(timestamp, '%H:%M:%S')
                 except ValueError:
                     print(f"Timestamp format error for command '{command}': {timestamp}")
-                    continue  
+                    continue
             
-            # formatted_timestamp = timestamp.strftime('%Y-%m-%d %H:%M:%S')
-            formatted_timestamp = timestamp
+            formatted_timestamp = timestamp.strftime('%H:%M:%S')
             writer.writerow([command, formatted_timestamp, coordinates])
 
     print(f"Command history saved to {filename}")
-
 
 def ReadLabelFile(file_path):
     with open(file_path, 'r') as f:
@@ -77,24 +86,17 @@ def DrawRectangles(request):
             else:
                 print("Invalid rectangle:", rect)
 
-async def InferenceTensorFlow(ws, result, image, model, output, label=None):
-    global rectangles, Detectnum ,Nothingnum, command_history
+async def InferenceTensorFlow(ws, result, image, output, label=None):
+    global rectangles, Detectnum ,Nothingnum, command_history,IsSteady,interpreter, input_details, output_details
     if label:
         labels = ReadLabelFile(label)
     else:
         labels = None
     
     start_time = time.time()
-    interpreter = tflite.Interpreter(model_path=model, num_threads=4)
-    interpreter.allocate_tensors()
-
-    input_details = interpreter.get_input_details()
-    output_details = interpreter.get_output_details()
     height = input_details[0]['shape'][1]
     width = input_details[0]['shape'][2]
-    floating_model = False
-    if input_details[0]['dtype'] == np.float32:
-        floating_model = True
+    floating_model = True
 
     # 檢查影像通道數，確保只有一個通道時才進行灰階轉換
     if len(image.shape) == 2:  # 如果是灰階影像
@@ -108,8 +110,14 @@ async def InferenceTensorFlow(ws, result, image, model, output, label=None):
     if floating_model:
         input_data = (np.float32(input_data) - 127.5) / 127.5
 
+    start_time = time.time()
+    
     interpreter.set_tensor(input_details[0]['index'], input_data)
     interpreter.invoke()
+    
+    end_time = time.time()
+    processing_time = end_time - start_time
+
 
     detected_boxes = interpreter.get_tensor(output_details[1]['index'])
     detected_classes = interpreter.get_tensor(output_details[3]['index'])
@@ -117,7 +125,6 @@ async def InferenceTensorFlow(ws, result, image, model, output, label=None):
     num_boxes = interpreter.get_tensor(output_details[2]['index'])
 
     num_boxes = int(num_boxes[0])
-    print("new detect")
     
     for i in range(num_boxes):
         box = detected_boxes[0][i]
@@ -126,50 +133,63 @@ async def InferenceTensorFlow(ws, result, image, model, output, label=None):
         score = detected_scores[0][i]
         Nothingnum += 1
         if score > 0.7:
-            Detectnum += 1
-            Nothingnum -= 10
-            ymin = top * lowresSize[1]
-            xmin = left * lowresSize[0]
-            ymax = bottom * lowresSize[1]
-            xmax = right * lowresSize[0]
+            # Detectnum += 1
+            Nothingnum = 0
+            ymin = top * normalSize[1]
+            xmin = left * normalSize[0]
+            ymax = bottom * normalSize[1]
+            xmax = right * normalSize[0]
 
             if labels:
                 print(f"  Label: {labels[classId]}, Score = {score}")
+                print(f"  Coordinates in pixels: xmin = {xmin:.1f}, ymin = {ymin:.1f}, xmax = {xmax:.1f}, ymax = {ymax:.1f}")
                 result.label = labels[classId]
                 result.score = score
+                rectangles.append([xmin, ymin, xmax, ymax])
+                current_time = datetime.now().strftime('%H:%M:%S')
+                coordinates_message = f"X:{xmin:.1f}~{xmax:.1f} Y:{ymin:.1f}~{ymax:.1f}"
+                command_history.append((f"Data{i}", current_time, coordinates_message))
                 if out is not None:
-                    current_time = datetime.now()
-                    formatted_time = current_time
-                    cv2.putText(image, f'Time: {formatted_time}', (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
                     cv2.rectangle(image, (int(xmin), int(ymin)), (int(xmax), int(ymax)), (0, 255, 0), 2)
                     cv2.putText(image, f"{labels[classId]}: {score:.2f}", (int(xmin), int(ymin)-10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2) 
             else:
                 print(f"  Score = {score}")
                 result.score = score
-            print(f"  Coordinates in pixels: xmin = {xmin:.1f}, ymin = {ymin:.1f}, xmax = {xmax:.1f}, ymax = {ymax:.1f}")
             result.xmin, result.ymin = f"{xmin:.1f}", f"{ymin:.1f}"
             result.xmax, result.ymax = f"{xmax:.1f}", f"{ymax:.1f}"
 
-            rectangles.append([xmin, ymin, xmax, ymax])
-    
-    print(f"Detectnum:{Detectnum}")
-    print(f"Nothingnum:{Nothingnum}")
-    if Nothingnum >= 29:
-        print("Nothing R2")
-        await ws.send(WebsocketMsg(NAME, {"toSerial":
-            [ord("R"), ord("2"), 0, 0]}).to_json())
-        await asyncio.sleep(0.1)
-        Nothingnum = 0
 
-    if Detectnum >= 1:
-        await resultforControl(ws)
-        Detectnum = 0
-        rectangles = []
-    else:
-        print("controlFun not implemented")
+    print(f"Nothingnum:{Nothingnum}")
     
-    end_time = time.time()
-    processing_time = end_time - start_time
+    
+    if(IsSteady == False):
+        await resultforControl(ws)
+    # if Nothingnum >= 29:
+    #     print("Nothing R2")
+    #     Nothingnum = 0
+    #     IsSteady = False
+    #     coordinates_message = "X Y"
+    #     command_history.append(("NoR2", current_time, coordinates_message))
+    #     await ws.send(WebsocketMsg(NAME, {"toSerial":
+    #         [ord("1"), ord("2"), 0, 0]}).to_json())
+    #     await asyncio.sleep(0.1)
+    #     await ws.send(WebsocketMsg(NAME, {"toSerial":
+    #         [ord("R"), ord("1"), 0, 0]}).to_json())
+        
+        
+        
+
+    # if Detectnum >= 1:
+    #     Nothingnum = 0
+    #     Detectnum = 0
+    #     if(IsSteady == False):
+    #         await resultforControl(ws)
+    #     rectangles = []
+        
+    # else:
+    #     print("controlFun not implemented")
+    
+    
     print(f"模型辨識時間: {processing_time:.4f} seconds")
     return image
 
@@ -178,7 +198,8 @@ async def resultforControl(ws):
     Ymin = 0
     Xmax = 0
     Ymax = 0
-    global IsSteady, already_up, already_down, command_history
+    global IsSteady ,already_up , already_down, command_history, rectangles
+    global last_Xmin, last_Ymin, last_Xmax, last_Ymax 
     for i in range(len(rectangles)):
         Xmin += rectangles[i][0]
         Ymin += rectangles[i][1]
@@ -191,102 +212,111 @@ async def resultforControl(ws):
         Ymax = Ymax / len(rectangles)
     Xmid = (Xmin + Xmax) / 2
     Ymid = (Ymin + Ymax) / 2
-    
-    current_time = datetime.now()
-    # formatted_time = current_time.strftime('%Y-%m-%d %H:%M:%S')
-    formatted_time = current_time
-    coordinates_message = f"X:{Xmin}~{Xmax}；Y:{Ymin}~{Ymax}"
     X_steadyzone_min = 280
     X_steadyzone_max = 440
     Y_steadyzone_min = 180
     Y_steadyzone_max = 300
 
-    x_adjusted = False
+    if last_Xmin is not None and last_Ymin is not None:
+        if abs(Xmin - last_Xmin) < 10 and abs(Ymin - last_Ymin) < 10 and abs(Xmax - last_Xmax) < 10 and abs(Ymax - last_Ymax) < 10:
+            print("skip control")
+            return
+    last_Xmid = (last_Xmin + last_Xmax) / 2
+    last_Ymid = (last_Ymin + last_Ymax) / 2
+    
+    current_time = datetime.now().strftime('%H:%M:%S')
+    coordinates_message = f"X:{Xmin:.1f}~{Xmax:.1f} Y:{Ymin:.1f}~{Ymax:.1f}"
     if Xmid < X_steadyzone_min: 
         print("L")
         IsSteady = False
+        command_history.append(("Left", current_time, coordinates_message))
         await ws.send(WebsocketMsg(NAME, {"toSerial":
             [ord("L"), ord("1"), 0, 0]}).to_json())
-        command_history.append(("L", formatted_time, coordinates_message))
         await asyncio.sleep(0.1)
-        x_adjusted = True
     elif Xmid > X_steadyzone_max: 
         print("R")
         IsSteady = False
+        command_history.append(("Right", current_time, coordinates_message))
         await ws.send(WebsocketMsg(NAME, {"toSerial":
             [ord("R"), ord("1"), 0, 0]}).to_json())
-        command_history.append(("R", formatted_time, coordinates_message))
         await asyncio.sleep(0.1)
-        x_adjusted = True
+    
+    # if Ymid < Y_steadyzone_min: 
+    #     print("U")
+    #     command_history.append(("Up", current_time, coordinates_message))
+    #     if already_up == False:
+    #         IsSteady = False
+    #         await ws.send(WebsocketMsg(NAME, {"toSerial":
+    #             [ord("U"), 0, 0, 0]}).to_json())
+    #         already_up = True
+    #     else:
+    #         print("Already Up")
+            
+    # elif Ymid > Y_steadyzone_max:
+    #     print("D")
+    #     command_history.append(("Down", current_time, coordinates_message))
+    #     if already_down == False:
+    #         IsSteady = False
+    #         await ws.send(WebsocketMsg(NAME, {"toSerial":
+    #             [ord("D"), 0, 0, 0]}).to_json())
+    #         already_down = True
+    #     else:
+    #         print("Already Down")
+    # else:
+    #     already_up = False
+    #     already_down = False
+    #     print("Balance")
+    #     command_history.append(("Balance", current_time, coordinates_message))
+    #     await ws.send(WebsocketMsg(NAME, {"toSerial":
+    #         [ord("M"), 0, 0, 0]}).to_json())
 
-    if x_adjusted:
-        await asyncio.sleep(0.1)
-
-    if Ymid < Y_steadyzone_min: 
-        print("U")
-        if already_up == False:
-            IsSteady = False
-            await ws.send(WebsocketMsg(NAME, {"toSerial":
-                [ord("U"), 0, 0, 0]}).to_json())
-            command_history.append(("U", formatted_time, coordinates_message))
-            already_up = True
-        else:
-            print("Already Up")
-    elif Ymid > Y_steadyzone_max:
-        print("D")
-        if already_down == False:
-            IsSteady = False
-            await ws.send(WebsocketMsg(NAME, {"toSerial":
-                [ord("D"), 0, 0, 0]}).to_json())
-            command_history.append(("D", formatted_time, coordinates_message))
-            already_down = True
-        else:
-            print("Already Down")
-    else:
-        already_up = False
-        already_down = False
-        print("Balance")
-        await ws.send(WebsocketMsg(NAME, {"toSerial":
-            [ord("B"), 0, 0, 0]}).to_json())
-        command_history.append(("B", formatted_time, coordinates_message))
-
-    if X_steadyzone_min <= Xmid <= X_steadyzone_max and Y_steadyzone_min <= Ymid <= Y_steadyzone_max:
+    if X_steadyzone_min <= (last_Xmid+Xmid)/2 <= X_steadyzone_max and Y_steadyzone_min <= (last_Ymid+Ymid)/2 <= Y_steadyzone_max:
         if IsSteady == False:
-            print("Steady - No Movement")
+            print("Steady - 已停止")
+            command_history.append(("Stop", current_time, coordinates_message))
             await ws.send(WebsocketMsg(NAME, {"toSerial":
-                [ord("X"), 0, 0, 0]}).to_json())
-            command_history.append(("X", formatted_time, coordinates_message))
-            IsSteady = True
-        else:
-            print("Steady Already")
-
-    if Xmax - Xmin > 576 and Ymax - Ymin > 384:  # 座標面積在整個鏡頭的80%以上就微小擺動直走
-        if IsSteady == False:
-            print("3")
-            await ws.send(WebsocketMsg(NAME, {"toSerial":
-                [ord("3"), 0, 0, 0]}).to_json())
-            command_history.append(("3", formatted_time, coordinates_message))
+                [ord("3"), 0, 0, 0]}).to_json()) 
             await asyncio.sleep(0.1)
             await ws.send(WebsocketMsg(NAME, {"toSerial":
-                [ord("2"), 0, 0, 0]}).to_json())
-            command_history.append(("2", formatted_time, coordinates_message))
+                [ord("X"), 0, 0, 0]}).to_json())
             IsSteady = True
+            
+            await asyncio.sleep(0.5)
+            await ws.send(WebsocketMsg(NAME, {"toSerial":
+                [ord("1"), ord("0"), 0, 0]}).to_json())
+            await asyncio.sleep(0.1)
+            await ws.send(WebsocketMsg(NAME, {"toSerial":
+                        [ord("!"), ord("0"), 0, 0]}).to_json())
+            IsSteady = False 
         else:
-            print("Already !")
+            print("Steady Already")
+            
+    last_Xmin, last_Ymin, last_Xmax, last_Ymax = Xmin, Ymin, Xmax, Ymax
+    rectangles = []
+    current_time = datetime.now().strftime('%H:%M:%S')
+    print(f"開始動作:{current_time}")
+    await asyncio.sleep(0.1)
 
 
-async def recognitionLoop(recoResult, ws): #可以正常錄影但辨識只會執行一次
+
+async def recognitionLoop(recoResult, ws):
     global out
     picam2 = Picamera2()
     config = picam2.create_preview_configuration(main={"size": normalSize},
                                                  lores={"size": lowresSize, "format": "YUV420"})
     picam2.configure(config)
-
-    stride = picam2.stream_configuration("lores")["stride"]
     picam2.post_callback = DrawRectangles
 
     picam2.start()
-
+    await InitializeModel(modelPath)
+    current_time = datetime.now().strftime('%H:%M:%S')
+    coordinates_message = f"begin"
+    command_history.append(("!", current_time, coordinates_message))
+    await ws.send(WebsocketMsg(NAME, {"toSerial":
+                [ord("1"), ord("0"), 0, 0]}).to_json())
+    await asyncio.sleep(0.1)
+    await ws.send(WebsocketMsg(NAME, {"toSerial":
+                [ord("!"), ord("0"), 0, 0]}).to_json())
     fourcc = cv2.VideoWriter_fourcc(*'XVID')
     frame_size = (normalSize[0], normalSize[1])
     out = cv2.VideoWriter(f"{datetime.now().strftime('%Y%m%d_%H:%M:%S')}.avi", fourcc, 20.0, frame_size)
@@ -295,23 +325,20 @@ async def recognitionLoop(recoResult, ws): #可以正常錄影但辨識只會執
         print("VideoWriter 無法開啟。")
         return
 
-    latest_detection_frame = None  # 用來存儲辨識結果
+    latest_detection_frame = None  
 
-    # 非同步執行辨識並儲存結果
+
     async def perform_inference(rgb_frame, frame_time):
         nonlocal latest_detection_frame
-        print("影像開始辨識")
         frame_with_detections = await InferenceTensorFlow(ws, recoResult, rgb_frame, modelPath, outputName, labelPath)
-        latest_detection_frame = (frame_with_detections, frame_time)  # 存儲辨識結果與時間
+        latest_detection_frame = (frame_with_detections, frame_time)  
 
     try:
-        # 開始初次辨識
         current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
         buffer = picam2.capture_buffer("lores")
         grey = buffer[:picam2.stream_configuration("lores")["stride"] * lowresSize[1]].reshape((lowresSize[1], picam2.stream_configuration("lores")["stride"]))
         rgb = cv2.cvtColor(grey, cv2.COLOR_GRAY2BGR)
-        asyncio.create_task(perform_inference(rgb, current_time))  # 啟動第一輪辨識
-        print(f"第一次啟動時間:{current_time}")
+        asyncio.create_task(perform_inference(rgb, current_time))  
 
         while True:
             start_time = time.time()
@@ -319,36 +346,29 @@ async def recognitionLoop(recoResult, ws): #可以正常錄影但辨識只會執
             buffer = picam2.capture_buffer("lores")
             grey = buffer[:picam2.stream_configuration("lores")["stride"] * lowresSize[1]].reshape((lowresSize[1], picam2.stream_configuration("lores")["stride"]))
             rgb = cv2.cvtColor(grey, cv2.COLOR_GRAY2BGR)
-
-            # 獲取當前幀的時間戳
             current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-            print(f"啟動時間:{current_time}")
-            # 檢查是否有更新的辨識結果，並將結果插入
+            print(f"迴圈開始時間:{current_time}")
             frame_with_detections = rgb
             if latest_detection_frame is not None:
                 detection_frame, detection_time = latest_detection_frame
-                if detection_time == current_time:
+                if detection_time != None:
                     frame_with_detections = detection_frame
-                    latest_detection_frame = None  # 清除已插入的結果
+                    latest_detection_frame = None  
 
-            # 調整幀大小並添加時間戳
+
             frame_with_detections = cv2.resize(frame_with_detections, frame_size)
             cv2.putText(frame_with_detections, current_time, (10, 20), cv2.FONT_HERSHEY_SIMPLEX,
-                        fontScale=0.5, color=(255, 255, 255), thickness=1)
+                        fontScale=0.5, color=(255, 165, 0), thickness=1)
 
-            # 寫入影像到影片
             out.write(frame_with_detections)
 
-            # 檢查辨識是否完成，如果完成則啟動下一輪辨識
             if latest_detection_frame is None:
-                # 辨識處理完成，抓取當前幀進行下一輪辨識
                 current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
                 buffer = picam2.capture_buffer("lores")
                 grey = buffer[:picam2.stream_configuration("lores")["stride"] * lowresSize[1]].reshape((lowresSize[1], picam2.stream_configuration("lores")["stride"]))
                 rgb = cv2.cvtColor(grey, cv2.COLOR_GRAY2BGR)
                 asyncio.create_task(perform_inference(rgb, current_time))  # 啟動新一輪辨識
 
-            # 確保錄影幀速率穩定
             elapsed_time = time.time() - start_time
             await asyncio.sleep(max(0, (1 / 20.0) - elapsed_time))  # 維持每秒 20 幀
     except KeyboardInterrupt:
@@ -359,6 +379,15 @@ async def recognitionLoop(recoResult, ws): #可以正常錄影但辨識只會執
             out.release()
         save_command_history_to_csv()
         print("影片已保存")
+
+
+
+
+
+
+
+
+
 
 
 
